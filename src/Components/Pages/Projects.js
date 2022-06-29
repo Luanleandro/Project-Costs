4390eef4c615f3b9f8d465ef7bf62dc83b42043e
Luan Leandro <luan.barbosacruz@gmail.com>
2022-06-29 19:22:26 -0300

    First commit

--- src/Components/Pages/Projects.js
+++ src/Components/Pages/Projects.js
@@ -1,4 +1,4 @@
-import { useLocation, useParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 import Message from "../layout/Message";
 import Container from "../layout/Container";
